0592c03125a7f6c43decc1ba205a0f7b7c72bc74
olex <olex@olexdeMacBook-Air.local>
2017-11-15 01:04:36 +0800

    first commit

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+
+
+app.get('/',function (req,res) {
+	res.send('<h1>Hello  d world</h1>')
+})
+
+
+app.listen(9093,function () {
+	console.log('start at port 9093')
+});
+
