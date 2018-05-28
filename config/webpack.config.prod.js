894b1b57f8e58d2608384370e593ce33072f85dc
olex <olex1216@gmail.com>
2018-05-28 10:20:24 +0800

    Update webpack.config.prod.js

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -124,7 +124,7 @@ module.exports = {
             options: {
               formatter: eslintFormatter,
               eslintPath: require.resolve('eslint'),
-
+              
             },
             loader: require.resolve('eslint-loader'),
           },
@@ -142,7 +142,7 @@ module.exports = {
             test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
             loader: require.resolve('url-loader'),
             options: {
-              limit: 1024,
+              limit: 10000,
               name: 'static/media/[name].[hash:8].[ext]',
             },
           },
@@ -152,7 +152,7 @@ module.exports = {
             include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-
+              
               compact: true,
             },
           },
@@ -169,7 +169,8 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.(less|scss|css)$/,
+            include: [/node_modules|antd-mobile/,path.resolve(__dirname, '../src/styles')],
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
@@ -207,6 +208,62 @@ module.exports = {
                           }),
                         ],
                       },
+                    },
+                    {
+                      loader: require.resolve('less-loader') // compiles Less to CSS
+                    },
+                  ],
+                },
+                extractTextPluginOptions
+              )
+            ),
+            // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
+          },
+          {
+            test: /\.(less|scss|css)$/,
+            exclude: [/node_modules|antd-mobile/,path.resolve(__dirname, '../src/styles')],
+            loader: ExtractTextPlugin.extract(
+              Object.assign(
+                {
+                  fallback: {
+                    loader: require.resolve('style-loader'),
+                    options: {
+                      hmr: false,
+                    },
+                  },
+                  use: [
+                    {
+                      loader: require.resolve('css-loader'),
+                      options: {
+                        importLoaders: 1,
+                        minimize: true,
+                        sourceMap: shouldUseSourceMap,
+                        modules:true,
+                        localIdentName:'[name]-[local]-[hash:base64:8]',
+                      },
+                    },
+                    {
+                      loader: require.resolve('postcss-loader'),
+                      options: {
+                        // Necessary for external CSS imports to work
+                        // https://github.com/facebookincubator/create-react-app/issues/2677
+                        ident: 'postcss',
+                        plugins: () => [
+                          require('postcss-flexbugs-fixes'),
+                          autoprefixer({
+                            browsers: [
+                              '>1%',
+                              'last 4 versions',
+                              'Firefox ESR',
+                              'not ie < 9', // React doesn't support IE8 anyway
+                            ],
+                            flexbox: 'no-2009',
+                          }),
+                        ],
+                      },
+                    },
+                    {
+                      loader: require.resolve('less-loader') // compiles Less to CSS
                     },
                   ],
                 },
